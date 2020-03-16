b3244235ab3e729d70e85c56bd40ae819a7f10d1
sidhu1012 <45732670+sidhu1012@users.noreply.github.com>
2020-03-16 15:21:47 +0530

    Add files via upload

---
+++ knn.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
+import pandas as pd
+import numpy as np
+import matplotlib.ticker as ticker
+from sklearn import preprocessing
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[2]:
+
+
+df=pd.read_csv('https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/teleCust1000t.csv')
+df.head()
+
+
+# In[3]:
+
+
+df['custcat'].value_counts()
+
+
+# In[4]:
+
+
+df.hist(column='income', bins=50)
+
+
+# In[5]:
+
+
+df.columns
+
+
+# In[6]:
+
+
+X = df[['region', 'tenure','age', 'marital', 'address', 'income', 'ed', 'employ','retire', 'gender', 'reside']] .values
+
+
+# In[7]:
+
+
+y = df['custcat'].values
+
+
+# In[8]:
+
+
+X = preprocessing.StandardScaler().fit(X).transform(X.astype(float))
+
+
+# In[9]:
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=4)
+print ('Train set:', X_train.shape,  y_train.shape)
+print ('Test set:', X_test.shape,  y_test.shape)
+
+
+# In[16]:
+
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+Ks = 20
+mean_acc = np.zeros((Ks-1))
+std_acc = np.zeros((Ks-1))
+ConfustionMx = [];
+for n in range(1,Ks):
+      
+    neigh = KNeighborsClassifier(n_neighbors = n).fit(X_train,y_train)
+    yhat=neigh.predict(X_test)
+    mean_acc[n-1] = metrics.accuracy_score(y_test, yhat)
+
+    
+    std_acc[n-1]=np.std(yhat==y_test)/np.sqrt(yhat.shape[0])
+
+mean_acc
+
+
+# In[17]:
+
+
+plt.plot(range(1,Ks),mean_acc,'g')
+plt.fill_between(range(1,Ks),mean_acc - 1 * std_acc,mean_acc + 1 * std_acc, alpha=0.10)
+plt.legend(('Accuracy ', '+/- 3xstd'))
+plt.ylabel('Accuracy ')
+plt.xlabel('Number of Nabors (K)')
+plt.tight_layout()
+plt.show()
+
+
+# In[18]:
+
+
+print( "The best accuracy was with", mean_acc.max(), "with k=", mean_acc.argmax()+1)
+
+
+# In[ ]:
+
+
+
+
